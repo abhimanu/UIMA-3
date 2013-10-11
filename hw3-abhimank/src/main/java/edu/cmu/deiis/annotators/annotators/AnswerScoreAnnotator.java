05993d2516915c4e2bb1c3372ab4bba61309b950
abhimanu <abhimanyu.kumar@gmail.com>
2013-10-11 03:16:06 -0400

    commit pom with dependencies

--- hw3-abhimank/src/main/java/edu/cmu/deiis/annotators/annotators/AnswerScoreAnnotator.java
+++ hw3-abhimank/src/main/java/edu/cmu/deiis/annotators/annotators/AnswerScoreAnnotator.java
@@ -44,6 +44,8 @@ public class AnswerScoreAnnotator extends JCasAnnotator_ImplBase {
     FSIndex questionIndex = jcas.getAnnotationIndex(Question.type);
     Iterator questionIter = questionIndex.iterator();
     
+    FSIndex annotationIndex = jcas.getAnnotationIndex(NamedEntityMention.type);
+    
 //    Vector<NGram> questionNgrams = new Vector<NGram>();
     Question ques = (Question) questionIter.next();
 
